85543494156f01ca1bd3b6921f4ee10024bcd050
RayStrauss <114413846+RayStrauss@users.noreply.github.com>
2022-11-26 19:51:26 -0300

    Add files via upload

---
+++ js/calcula-imc.js
@@ -0,0 +1,60 @@
+var titulo = document.querySelector(".titulo");
+      titulo.textContent = "Nutrição Bonifácio";   
+
+var pacientes = document.querySelectorAll(".paciente");
+for(var i=0; i< pacientes.length; i++){
+var paciente  = pacientes[i];
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoValido = validaPeso(peso); //true ou false
+var alturaValida = validaAltura(altura);
+
+if (!pesoValido) {
+      console.log ("Peso Inválido");
+      pesoValido = false;
+      tdImc.textContent="Peso inválido";
+      paciente.classList.add("paciente-invalido");
+}
+
+if (!alturaValida) {
+      console.log("Altura Inválida");
+      alturaValida = false;
+      tdImc.textContent = "Altura inválida";
+      paciente.classList.add("paciente-invalido");
+}
+if (pesoValido && alturaValida) {
+      var imc = calculaImc(peso, altura);
+      tdImc.textContent = imc;
+      }else{
+      tdImc.textContent = "Altura e/ou peso inválidos";
+ }
+}
+
+function validaPeso(peso) {
+      if (peso >= 0 && peso < 1000) {
+            return true;
+      }else{
+            return false;
+      }
+}
+
+function validaAltura(altura) {
+      if (altura >= 0 && altura <= 3.0) {
+            return true;
+      }else{
+            return false;
+      }
+}
+
+function calculaImc(peso,altura){
+      var imc = 0;
+      imc = peso / (altura * altura);
+      return imc.toFixed(2);
+}
